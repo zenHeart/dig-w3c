a66239bdde3ab5ed64625a4dc0ec7f7e6abf7786
zenheart <locke.cheng@wpi-group.com>
2017-11-18 22:15:35 +0800

    添加 es6 yield 测试用例

--- demo/es6/es6.test.js
+++ demo/es6/es6.test.js
@@ -110,16 +110,21 @@ describe('generator 测试',function(){
     it('yield 放在表达式或代码块前,申明函数的执行阶段',function() {
         function * sunMove() {
             yield 'sunrise';
+            yield ;//申明一个空的 yield 阶段返回 undefined
+            console.log((yield 'noon'));//yield 在另一个表达式中必须包含括号
             yield 'sundown';
         }
         //实例化一个迭代器
         var g = sunMove();
 
-        //next 切换状态到
+        /*
+        * yield 申明函数阶段
+        * next 切换函数阶段*/
         expect(g.next(),'next 切换到第一个 yield 状态,返回对象,value 指代 yield 当前表达式的值,done 表示是否迭代结束').to.be.deep.equal({ value: 'sunrise', done: false });
-        expect(g.next(),'next 切换状态状态到 sundown').to.be.deep.equal({ value: 'sundown', done: false });
-        expect(g.next(),'next 执行结束,value 返回函数执行结果,没有则为 undefined').to.be.deep.equal({ value:undefined, done: true });
-        expect(g.next(),'执行结束后的迭代调用 next 返回相同结果').to.be.deep.equal({ value:undefined, done: true });
+        expect(g.next(),'next 空的 yiled 表达式 value 为 undefined').to.be.deep.equal({ value: undefined, done: false });
+        expect(g.next(),'yield 嵌套在其他表达使用必须带括号').to.be.deep.equal({ value: 'noon', done: false });
+        expect(g.next()).to.be.deep.equal({ value:'sundown', done: false });
+        expect(g.next(),'执行结束后的调用 next 返回 undefined').to.be.deep.equal({ value:undefined, done: true });
         expect(sunMove().next(),'重新实例化一个迭代器').to.be.deep.equal({ value: 'sunrise', done: false });
     })
 })
