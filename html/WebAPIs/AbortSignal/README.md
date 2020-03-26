2dcec5dedb4dd0ad3ba577e2ce4afc496083f470
zenheart <zenheart_register@163.com>
2020-03-26 10:16:28 +0800

    docs: 添加 undefined 使用说明

---
+++ html/WebAPIs/AbortSignal/README.md
@@ -0,0 +1,3 @@
+# [AbortSignal](https://developer.mozilla.org/zh-CN/docs/Web/API/AbortSignal)
+
+用于终止 DOM 请求。
--- js/ecma/1.language-feature/class/class.test.js
+++ js/ecma/1.language-feature/class/class.test.js
@@ -1,42 +1,49 @@
-const {expect,should,assert} = require('chai');
+const { expect, should, assert } = require('chai');
 
-describe('class 语法,底层仍采用原型链封装',function(){
-    it('class 申明类',function() {
-        class Person {
-            constructor(name) {
-                this.name = name;
-            } //声明类
+describe('class 语法,底层仍采用原型链封装', function() {
+  it('class 申明类', function() {
+    class Person {
+      constructor(name) {
+        this.name = name;
+      } //声明类
+    }
 
+    var person = new Person('zenheart');
+
+    expect(person).to.be.instanceOf(Person);
+    expect(person.name).to.be.equal('zenheart');
+  });
+  it('extends 定义继承模式', function() {
+    class Person {
+      constructor(name) {
+        this.name = name;
+      } //声明类
+    }
+    class Student extends Person {
+      constructor(name, school) {
+        super(name);
+        this.school = school;
+      } //声明类
+    }
+
+    var student = new Student('zenheart', 'test');
+
+    expect(student).to.be.instanceOf(Student);
+    expect(student.name).to.be.equal('zenheart');
+    expect(student.school).to.be.equal('test');
+  });
+  /* describe.skip('private', function() {
+    it('私有变量可以在类中访问', function() {
+      class Message {
+        #message;
+        constructor() {
+          this.#message = 'home';
         }
-
-        var person = new Person('zenheart');
-
-        expect(person).to.be.instanceOf(Person);
-        expect(person.name).to.be.equal('zenheart');
-    })
-    it('extends 定义继承模式',function() {
-        class Person {
-
-            constructor(name) {
-                this.name = name;
-            } //声明类
-
+        static greet() {
+          expect(this.#message).to.be('home1');
         }
-        class Student extends Person {
-            constructor(name,school) {
-                super(name);
-                this.school = school;
-            } //声明类
-        }
-
-        var student = new Student('zenheart','test');
-
-        expect(student).to.be.instanceOf(Student);
-        expect(student.name).to.be.equal('zenheart');
-        expect(student.school).to.be.equal('test');
-    })
-
+      }
+      expect(Message.greet());
+    });
+  }); */
 });
-
-
-
